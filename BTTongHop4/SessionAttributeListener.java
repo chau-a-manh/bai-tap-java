3d83ec6d68dce68ced133174a1be0e9b45b45a20
CHAU A MANH <145680331+chau-a-manh@users.noreply.github.com>
2025-05-13 11:34:26 +0700

    Add files via upload

---
+++ BTTongHop4/SessionAttributeListener.java
@@ -0,0 +1,21 @@
+import jakarta.servlet.annotation.WebListener;
+import jakarta.servlet.http.HttpSessionAttributeListener;
+import jakarta.servlet.http.HttpSessionBindingEvent;
+
+@WebListener
+public class SessionAttributeListener implements HttpSessionAttributeListener {
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent event) {
+        System.out.println("Attribute added: " + event.getName() + " = " + event.getValue());
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent event) {
+        System.out.println("Attribute removed: " + event.getName());
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent event) {
+        System.out.println("Attribute replaced: " + event.getName() + " = " + event.getValue());
+    }
+}
